0c41874ecc08c3963acb9ed10a044fd679441468
SamarV-121 <samarvispute121@gmail.com>
2019-10-21 07:49:06 +0000

    mt6765-common: Move props to /system/etc/prop.default
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: I2aebd05f4f38f7e33a50842cab5f45d048f3d10c

--- system_prop.mk
+++ system_prop.mk
@@ -1,25 +1,25 @@
 # system.prop for mt6765-common
 
 # Audio
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
    ro.config.vc_call_vol_steps=7 \
    ro.config.media_vol_steps=25
 
 # Performance
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mtk_perf_simple_start_win=1 \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
 
 # RIL
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vendor.rild.libpath=mtk-ril.so \
     vendor.rild.libargs=-d/dev/ttyC0
 
 # Surfaceflinger
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
    ro.surface_flinger.vsync_event_phase_offset_ns=8300000 \
-   ro.surface_flinger.vsync_sf_event_phase_offset_ns=830000 \0
+   ro.surface_flinger.vsync_sf_event_phase_offset_ns=830000 \
    ro.surface_flinger.present_time_offset_from_vsync_ns=0
