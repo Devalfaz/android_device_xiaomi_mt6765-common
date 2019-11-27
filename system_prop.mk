98d70789be98261e05636c0dd0a95c66ccd9ce4c
myst33d <45560476+myst33d@users.noreply.github.com>
2019-11-27 12:08:00 +0000

    mt6765-common: Correct Volume Levels
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,5 +1,12 @@
 # system.prop for mt6765-common
 
+# Audio
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+   ro.config.vc_call_vol_steps=7 \
+   ro.config.alarm_vol_steps=15 \
+   ro.config.system_vol_steps=15 \
+   ro.config.ring_notification_vol_steps=15
+
 # Performance
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mtk_perf_simple_start_win=1 \
